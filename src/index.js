612f955b26fb05d2d054e05c63b2b58fb7badc7b
Manit123001 <captain_new@hotmail.com>
2018-01-21 18:55:15 +0700

    finish section 1 An Intro to React

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAUt_0ARxklQMbZU8BO1f8EJ-S0cn5RHmI';
+
 
 // Create a new component. This component should produce
 // some HTMl
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+
 }
 
 // Take this component's generated HTML and put it 
 // on the page (in the DOM)
 ReactDom.render(<App />,
-    document.querySelector('.container'))
+    document.querySelector('.container'));
